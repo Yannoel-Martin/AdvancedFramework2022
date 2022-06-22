b213c3d38190ab74574a71cac96c919fab09bf3b
Yannoël <yannoel.martin@hotmail.fr>
2022-06-22 14:48:59 +0200

    exoA selon code

---
+++ exoA/java-web/java-web-project/src/main/java/academy/campus/web/WebInitializer.java
@@ -0,0 +1,25 @@
+package academy.campus.web;
+
+import academy.campus.web.config.SpringConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+private static final String ROOT_PATH = "/";
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {ROOT_PATH};
+    }
+
+}
